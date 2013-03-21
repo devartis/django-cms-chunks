22d5cc06b025983cd3b9578e22e9b788a53adbc8
lgomez <lgomez@devartis.com>
2013-03-21 14:00:19 -0300

    Removed extra field "veideo_placeholder"

--- cms_chunks/models.py
+++ cms_chunks/models.py
@@ -5,7 +5,6 @@ from cms.models.fields import PlaceholderField
 class Chunk(models.Model):
     tags = models.CharField(max_length=200)
     code = PlaceholderField('chunk_placeholder', related_name="chunks")
-    video_placeholder = PlaceholderField('video_placeholder', related_name="videos")
     priority = models.IntegerField()
 
 # Create your models here.
